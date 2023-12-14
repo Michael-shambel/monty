e96a5528442a03e1efb1e6ccae34a208e572b08a
NecheU <uzorgodsboy1@gmail.com>
2023-12-14 06:17:22 -0800

    swap_stack

--- monty.h
+++ monty.h
@@ -1,5 +1,8 @@
-#ifdef _MONTY_H_
+#ifndef _MONTY_H_
 #define _MONTY_H_
+
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
