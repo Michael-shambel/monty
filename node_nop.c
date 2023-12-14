770d751b675dc96ca86d5110fb15d1366fa19a6e
NecheU <uzorgodsboy1@gmail.com>
2023-12-14 14:04:23 -0800

    node_nop

--- node_nop.c
+++ node_nop.c
@@ -1,9 +1,10 @@
 #include "monty.h"
 
 /**
- *
- *
- *
+ * n_nop - Function to add teo element at too of a stack
+ * @stack: pointer to the stak
+ * @line_number: line of the nuber added
+ * Return: void
  */
 void n_nop(stack_t **stack, unsigned int line_number)
 {
