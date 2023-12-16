5023951016cb7d3d24fe4a0e11c1e541ddbed091
Michael Shambel <Michaelshambel10@gmail.com>
2023-12-16 01:06:16 -0800

    error solved

--- node_add.c
+++ node_add.c
@@ -1,18 +1,12 @@
 #include "monty.h"
 
 /**
-<<<<<<< HEAD
  *n_add - this is node help to add
  *@stack: Double pointer to the top of the stack.
  *@line_number: Line number in the Monty file
  *
  * Return: nothing
-=======
- * n_add - Function that adds element to the stack
- * @stack: pointer to the stack
- * @line_number: line number
- * Return: void
->>>>>>> c1e660b915e0ba5025787dd617ec019bd1983735
+ *
  */
 void n_add(stack_t **stack, unsigned int line_number)
 {
--- node_pop.c
+++ node_pop.c
@@ -1,8 +1,8 @@
 #include "monty.h"
 /**
- *
- *
- *
+ *n_pop - pop out the top of node
+ *@stack: Double pointer to the top of the stack
+ *@line_number: Line number in the Monty file
  *
  */
 void n_pop(stack_t **stack, unsigned int line_number)
